208157d8cdee86cd49a101b7033473bef338d770
Frank Stallone III <Frank.Stallone@adp.com>
2019-01-24 19:19:26 -0500

    feat: ✨ Officially v1

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -5,7 +5,7 @@ import tw from 'tailwind.macro'
 
 const Wrapper = styled.a`
   width: 100%;
-  ${tw`shadow-lg relative no-underline rounded-lg px-8 py-8 md:py-4 text-white`};
+  ${tw`shadow-lg relative no-underline rounded-lg px-8 py-8 md:py-4 m-16 text-white`};
   background: ${props => props.bg};
   transition: transform 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
   &:hover {
@@ -14,19 +14,21 @@ const Wrapper = styled.a`
 `
 
 const Text = styled.div`
-  ${tw`opacity-75 font-sans text-sm md:text-base`};
+  ${tw`font-sans text-sm md:text-base`};
   text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+  text-align: left;
 `
 
-const Title = styled.div`
+const Price = styled.div`
   ${tw`text-white uppercase text-2xl md:text-3xl xl:text-4xl tracking-wide font-sans pt-8`};
   text-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+  text-align: center;
 `
 
-const ProjectCard = ({ title, link, children, bg }) => (
-  <Wrapper href={link} target="_blank" rel="noopener noreferrer" bg={bg}>
+const ProjectCard = ({ title, children, bg }) => (
+  <Wrapper bg={bg}>
     <Text>{children}</Text>
-    <Title>{title}</Title>
+    <Price>{title}</Price>
   </Wrapper>
 )
 
@@ -34,7 +36,6 @@ export default ProjectCard
 
 ProjectCard.propTypes = {
   title: PropTypes.string.isRequired,
-  link: PropTypes.string.isRequired,
   children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]).isRequired,
   bg: PropTypes.string.isRequired,
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,17 +20,7 @@ import Contact from '../views/Contact'
 import avatar from '../images/avatar.png'
 
 const ProjectsWrapper = styled.div`
-  ${tw`flex flex-wrap justify-between mt-8`};
-  display: grid;
-  grid-gap: 4rem;
-  grid-template-columns: repeat(2, 1fr);
-  @media (max-width: 1200px) {
-    grid-gap: 3rem;
-  }
-  @media (max-width: 900px) {
-    grid-template-columns: 1fr;
-    grid-gap: 2rem;
-  }
+  ${tw`flex flex-wrap justify-between mt-8 max-w-md`};
 `
 
 const AboutHero = styled.div`
@@ -63,18 +53,15 @@ const Index = () => (
     <Parallax pages={5}>
       <Hero offset={0}>
         <BigTitle>Hoverboard Media</BigTitle>
-        <Subtitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</Subtitle>
+        <Subtitle>Providing the benefits of enterprise grade WordPress hosting on small business budgets.</Subtitle>
       </Hero>
       <Projects offset={1}>
-        <Title>Service</Title>
+        <Title>Enterprise Grade WordPress Hosting</Title>
         <ProjectsWrapper>
           <ProjectCard
             title="$840.00 per year"
-            link="#"
             bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
           >
-            <h1>Enterprise Grade WordPress Hosting</h1>
-            <p>The benefits of enterprise grade WordPress hosting on small business budgets.</p>
             <p>One year of WordPress hosting, performance, security and maintenance.</p>
             <ul>
               <li>25k monthly views / 125K pages served</li>
@@ -97,33 +84,25 @@ const Index = () => (
       <About offset={3}>
         <Title>About</Title>
         <AboutHero>
-          <Avatar src={avatar} alt="John Doe" />
+          <Avatar src={avatar} alt="Frank Stallone" />
           <AboutSub>
-            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
-            Emoji into my speech to better express myself. Winky face.
+            Howdy! I am Frank Stallone and I have been working with WordPress for 10 years. Over that time I have refined my strategy for design, development, performance, security, maintenance, team collaboration and more.
           </AboutSub>
         </AboutHero>
         <AboutDesc>
-          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
-          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
-          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
-          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
-          right?
+           As a small business owner I believe your small business deserves the same advantages as large enterprise applications. This premium WordPress hosting offering is it. Let me handle your WordPress hosting, so you can work on your business. Let's migrate your site today.
         </AboutDesc>
       </About>
       <Contact offset={4}>
         <Inner>
-          <Title>Get in touch</Title>
+          <Title>Ready to get started?</Title>
           <ContactText>
-            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
-            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
-            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+            Get in touch <a href="mailto:iamfrankstallone@gmail.com">by email</a> or on{' '}
+            <a href="https://twitter.com/frankstallone">Twitter</a>
           </ContactText>
         </Inner>
         <Footer>
-          &copy; 2019 by Gatsby Starter Portfolio Cara.{' '}
-          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>. Made by{' '}
-          <a href="https://www.lekoarts.de">LekoArts</a>.
+          &copy; 2019 by{' '}<a href="https://iamfrankstallone.com">Frank Stallone</a>.
         </Footer>
       </Contact>
     </Parallax>
